c22d5d204ea7376c5a9c24b80ae3ccfa8a7a9b89
Corey Ladovsky <coreyskymusic@gmail.com>
2018-02-27 16:33:27 -0500

    album detail

--- albums/src/components/header.js
+++ albums/src/components/header.js
@@ -23,7 +23,6 @@ const styles = {
     height: 60,
     paddingTop: 15,
     shadowColor: "black",
-    shadowOffeset: { width: 0, height: 2 },
     shadowOpacity: 0.2,
     elevation: 2,
     position: 'relative'
