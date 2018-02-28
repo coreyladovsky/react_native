7e9f57dc0913ac9b241781a5497c6a136aabe49f
Corey Ladovsky <coreyskymusic@gmail.com>
2018-02-28 16:30:11 -0500

    albums and scrollView

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -4,9 +4,9 @@ import Card from './card';
 import CardSection from './CardSection';
 
 const AlbumDetail = ({ album }) => {
-  const { title, artist, thumbnail_image } = album;
+  const { title, artist, thumbnail_image, image } = album;
   const { thumbnailStyle, headerContentStyle, thumbnailContainerStyle,
-  headerTextStyle } = styles;
+  headerTextStyle, imageStyle } = styles;
   return(
     <Card>
       <CardSection>
@@ -17,6 +17,9 @@ const AlbumDetail = ({ album }) => {
          <Text style={headerTextStyle}>{title}</Text>
          <Text>{artist}</Text>
          </View>
+      </CardSection>
+      <CardSection>
+        <Image style={imageStyle} source={{uri: image}}/>
       </CardSection>
     </Card>
   );
@@ -40,7 +43,13 @@ const styles = {
     alignItems: 'center',
     marginLeft: 10,
     marginRight: 10
-  }
+  },
+
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
+  },
 };
 
 export default AlbumDetail;
