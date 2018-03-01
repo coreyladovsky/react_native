a64fea95eb39fd6a4dded32e84fe9ef0477bdccf
Corey Ladovsky <coreyskymusic@gmail.com>
2018-03-01 13:49:36 -0500

    moved components and setting up firebase

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
+import { Header } from './components/common';
 
 class App extends Component {
+
+  componentWillMount() {
+    firebase.initializeApp({
+    apiKey: 'AIzaSyBM-a620FOBwSHSjWrfeV_cJjYEDXiwTDY',
+    authDomain: 'auth-e8e57.firebaseapp.com',
+    databaseURL: 'https://auth-e8e57.firebaseio.com',
+    projectId: 'auth-e8e57',
+    storageBucket: 'auth-e8e57.appspot.com',
+    messagingSenderId: '864429669522'
+  });
+  }
+
   render() {
     return(
       <View>
+        <Header headerText="Authentication"/>
         <Text>Hey Corey</Text>
       </View>
     );
   }
 }
 
-export default App; 
+export default App;
