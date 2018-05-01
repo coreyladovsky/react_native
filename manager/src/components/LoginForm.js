c0626eb2223e1ae6db5afdaa821824acfb770044
Corey Ladovsky <coreyskymusic@gmail.com>
2018-05-01 14:18:52 -0400

    login form appearing

---
+++ manager/src/components/LoginForm.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Card, CardSection, Input, Button } from './common';
+
+class LoginForm extends React.Component {
+  render () {
+    return(
+      <Card>
+        <CardSection>
+        <Input label="Email" placeholder="email@gmail.com"/>
+        </CardSection>
+
+        <CardSection>
+        <Input secureTextEntry label="Password" placeholder="********"/>
+
+        </CardSection>
+
+        <CardSection>
+          <Button>
+            Login
+          </Button>
+        </CardSection>
+
+      </Card>
+    );
+  }
+}
+
+export default LoginForm;
