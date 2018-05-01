94023281712e51f8f6a2052b90a8388f56a76b46
Corey Ladovsky <coreyskymusic@gmail.com>
2018-05-01 14:12:39 -0400

    added commons

---
+++ manager/src/components/common/Header.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Header = (props) => {
+  const { textStyle, viewStyle } = styles;
+  return(
+    <View style={viewStyle}>
+      <Text style={textStyle} >{props.headerText}</Text>
+    </View>
+  );
+};
+
+const styles = {
+  textStyle: {
+    fontSize: 20,
+    color: "green"
+
+  },
+  viewStyle: {
+    backgroundColor: "yellow",
+    alignItems: "center",
+    justifyContent: "center",
+    height: 60,
+    paddingTop: 15,
+    shadowColor: "black",
+    shadowOpacity: 0.2,
+    elevation: 2,
+    position: 'relative'
+  }
+};
+
+export { Header} ;
