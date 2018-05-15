d1ae6f55d749ea1d8851cd5691c011d0f6941453
Corey Ladovsky <coreyskymusic@gmail.com>
2018-05-15 12:07:17 -0400

    clearing form on success

--- manager/src/components/EmployeeCreate.js
+++ manager/src/components/EmployeeCreate.js
@@ -2,50 +2,84 @@ import React from "react";
 import { Text, Picker } from "react-native";
 import { Card, CardSection, Input, Button } from "./common";
 import { connect } from "react-redux";
-import { employeeUpdate } from "../actions/";
+import { employeeUpdate, employeeCreate } from "../actions/";
 
 class EmployeeCreate extends React.Component {
+  onButtonPress() {
+    const { name, phone, shift } = this.props;
+    this.props.employeeCreate({ name, phone, shift: shift || 'Monday' });
+  }
+
   render() {
     return (
       <Card>
-        <Text style={{ paddingTop: 55 }} />
+        <Text style={{ paddingTop: 50 }} />
 
         <CardSection>
-          <Input label="Name" placeholder="Jill" value={this.props.name} onChangeText={(value) => this.props.employeeUpdate({prop: 'name', value})}/>
+          <Input
+            label="Name"
+            placeholder="Jill"
+            value={this.props.name}
+            onChangeText={value =>
+              this.props.employeeUpdate({ prop: "name", value })
+            }
+          />
         </CardSection>
 
         <CardSection>
-          <Input label="Phone" placeholder="555-555-5555" value={this.props.phone} onChangeText={(value) => this.props.employeeUpdate({prop: 'phone', value})}/>
+          <Input
+            label="Phone"
+            placeholder="555-555-5555"
+            value={this.props.phone}
+            onChangeText={value =>
+              this.props.employeeUpdate({ prop: "phone", value })
+            }
+          />
         </CardSection>
 
-        <CardSection>
-        <Picker
-        style={{flex: 1}}
-        selectedValue={this.props.shift}
-        onValueChange={(value) => this.props.employeeUpdate({prop: 'shift', value})}
-        >
-          <Picker.Item label="Monday" value="Monday" />
-          <Picker.Item label="Tuesday" value="Tuesday" />
-          <Picker.Item label="Wednesday" value="Wednesday" />
-          <Picker.Item label="Thursday" value="Thursday" />
-          <Picker.Item label="Friday" value="Friday" />
-          <Picker.Item label="Saturday" value="Saturday" />
-          <Picker.Item label="Sunday" value="Sunday" />
-        </Picker>
+        <CardSection style={{ flexDirection: "column" }}>
+          <Text style={styles.pickerTextStyle}>Shift</Text>
+          <Picker
+            selectedValue={this.props.shift}
+            onValueChange={value =>
+              this.props.employeeUpdate({ prop: "shift", value })
+            }
+          >
+            <Picker.Item label="Monday" value="Monday" />
+            <Picker.Item label="Tuesday" value="Tuesday" />
+            <Picker.Item label="Wednesday" value="Wednesday" />
+            <Picker.Item label="Thursday" value="Thursday" />
+            <Picker.Item label="Friday" value="Friday" />
+            <Picker.Item label="Saturday" value="Saturday" />
+            <Picker.Item label="Sunday" value="Sunday" />
+          </Picker>
         </CardSection>
         <CardSection>
-          <Button>Create Employee</Button>
+          <Button onPress={this.onButtonPress.bind(this)}>
+            Create Employee
+          </Button>
         </CardSection>
       </Card>
     );
   }
 }
 
+const styles = {
+  pickerTextStyle: {
+    fontSize: 18,
+    paddingLeft: 20
+  }
+};
+
 const mapStateToProps = state => {
   const { name, phone, shift } = state.employeeForm;
   return {
-    name, phone, shift
+    name,
+    phone,
+    shift
   };
 };
 
-export default connect(mapStateToProps, { employeeUpdate })(EmployeeCreate);
+export default connect(mapStateToProps, { employeeUpdate, employeeCreate })(
+  EmployeeCreate
+);
--- manager/src/reducers/EmployeeFormReducer.js
+++ manager/src/reducers/EmployeeFormReducer.js
@@ -1,4 +1,4 @@
-import { EMPLOYEE_UPDATE } from '../actions/types';
+import { EMPLOYEE_UPDATE, EMPLOYEE_CREATE } from '../actions/types';
 
 const INITIAL_STATE ={
   name: '',
@@ -6,10 +6,11 @@ const INITIAL_STATE ={
   shift: ''
 }
 export default  (state = INITIAL_STATE, action) => {
-  console.log(action);
   switch (action.type) {
     case EMPLOYEE_UPDATE:
         return {...state, [action.payload.prop]: action.payload.value };
+    case EMPLOYEE_CREATE:
+      return INITIAL_STATE;
     default:
       return state;
 
