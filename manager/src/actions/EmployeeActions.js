cb462a0295c3c1c1829f85f2896f5c45460980cf
Corey Ladovsky <coreyskymusic@gmail.com>
2018-05-16 13:10:07 -0400

    working edit

--- manager/src/actions/EmployeeActions.js
+++ manager/src/actions/EmployeeActions.js
@@ -1,5 +1,5 @@
 import firebase from 'firebase';
-import {EMPLOYEE_UPDATE, EMPLOYEE_CREATE, EMPLOYEES_FETCH_SUCCESS  } from './types';
+import {EMPLOYEE_UPDATE, EMPLOYEE_CREATE, EMPLOYEES_FETCH_SUCCESS, EMPLOYEE_SAVE_SUCCESS  } from './types';
 import { Actions } from 'react-native-router-flux';
 
 export const employeeUpdate = ({prop, value}) => {
@@ -36,9 +36,14 @@ export const employeesFetch = () => {
 
 export const employeeSave = ({name, phone, shift, uid}) => {
   const {currentUser} = firebase.auth();
-  return () => {
+  return (dispatch) => {
       firebase.database().ref(`/users/${currentUser.uid}/employees/${uid}`)
       .set({name, phone, shift })
-      .then(() => console.log('saved!'));
+      .then(() => {
+        dispatch({type: EMPLOYEE_SAVE_SUCCESS})
+        Actions.employeeList({type: 'reset'});
+
+
+    });
   };
 };
